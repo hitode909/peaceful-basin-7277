54f19b9709e88772928b169be2edfe1d6106a885
hitode909 <hitode909@gmail.com>
2015-05-07 15:20:32 +0900

    random response

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.status(200).send(Math.random()+'').end();
 });
 
 app.listen(app.get('port'), function() {
