dbe0bc37b4eab5fa934b69560b73054d31f017ec
hitode909 <hitode909@gmail.com>
2015-05-07 15:27:44 +0900

    use string_random

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
 
+var String_random = require('string_random.js').String_random;
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.status(200).send(Math.random()+'').end();
+  response.status(200).send(String_random(".+")).end();
 });
 
 app.listen(app.get('port'), function() {
